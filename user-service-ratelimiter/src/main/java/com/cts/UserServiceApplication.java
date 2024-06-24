fd4075237e237894c9f8a4dceeed35b151eef079
mithilesh108 <tech.mithilesh108@gmail.com>
2024-06-24 22:55:29 +0530

    resilence4j example

---
+++ user-service-ratelimiter/src/main/java/com/cts/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableAspectJAutoProxy
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
